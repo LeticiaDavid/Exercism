c52fc3555361f801c7a3b8d9ff762f1850209c98
Leticia David <leticia.david17@gmail.com>
2023-02-28 18:43:38 -0300

    Sixth exercise

---
+++ fsharp/valentines-day/ValentinesDay.fs
@@ -0,0 +1,40 @@
+module ValentinesDay
+
+type Approval = 
+    | Yes
+    | No
+    | Maybe
+
+type Cuisine = 
+    | Korean
+    | Turkish
+
+type Genre = 
+    | Crime
+    | Horror
+    | Romance
+    | Thriller
+
+type Activity = 
+    | BoardGame
+    | Chill
+    | Movie of Genre
+    | Restaurant of Cuisine
+    | Walk of int
+
+let rateActivity (activity: Activity): Approval = 
+    match activity with
+    | BoardGame -> No
+    | Chill -> No
+    | Movie Crime -> No
+    | Movie Horror -> No
+    | Movie Romance -> Yes
+    | Movie Thriller -> No
+    | Restaurant Korean -> Yes
+    | Restaurant Turkish -> Maybe
+    | Walk(0) -> Yes
+    | Walk(1) -> Yes
+    | Walk(2) -> Yes
+    | Walk(3) -> Maybe
+    | Walk(4) -> Maybe
+    | Walk(_) -> No
