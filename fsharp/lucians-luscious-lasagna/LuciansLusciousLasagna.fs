62c27d4563ef97c115d98f0a7d540e14f6ff7fa2
Leticia David <leticia.david17@gmail.com>
2023-02-28 18:26:40 -0300

    Second exercise

---
+++ fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -0,0 +1,9 @@
+module LuciansLusciousLasagna
+
+let expectedMinutesInOven = 40
+
+let remainingMinutesInOven x  =  expectedMinutesInOven - x
+
+let preparationTimeInMinutes y =  y * 2
+
+let elapsedTimeInMinutes z  w =  (z * 2) + w
